68a9af2fd350e4ea68870eaf19bd10c98c9af1df
David Hérault <dherault@gmail.com>
2022-10-13 17:05:17 +0200

    Edit GradientButton

--- app/src/components/GradientButton.tsx
+++ app/src/components/GradientButton.tsx
@@ -3,16 +3,16 @@ import { Button, Div } from 'honorable'
 function GradientButton({ gradient = 'gold', fill = false, ...props }) {
   return (
     <Div
-      borderRadius="medium"
+      borderRadius="large"
       background={gradient}
       _active={{ transform: 'scale(1.05)' }}
-      transition="all 200ms ease"
+      transition="all 150ms ease"
       cursor="pointer"
       p={0.25}
     >
       <Button
         backgroundColor={fill ? 'transparent' : 'background'}
-        borderRadius="medium"
+        borderRadius="large"
         _hover={{ backgroundColor: 'transparent' }}
         _active={{ backgroundColor: 'transparent' }}
         {...props}

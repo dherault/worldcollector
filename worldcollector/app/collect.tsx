043f00a9ae897cdce29f0a13bcd4dbea258f19cc
David Hérault <dherault@gmail.com>
2023-06-05 22:03:13 +0200

    Add collect page

--- worldcollector/app/collect.tsx
+++ worldcollector/app/collect.tsx
@@ -1,9 +1,42 @@
-import { Box } from 'native-base'
+import { useRouter } from 'expo-router'
+import { Box, HStack, Heading, Icon, IconButton } from 'native-base'
+import { MaterialIcons } from '@expo/vector-icons'
 
 function Collect() {
+  const router = useRouter()
+
   return (
-    <Box>
-      Collect
+    <Box
+      pt={4}
+      position="relative"
+    >
+
+      <HStack justifyContent="center">
+        <Heading size="xl">Collect</Heading>
+      </HStack>
+      <Box
+        position="absolute"
+        top={3}
+        right={3}
+      >
+        <IconButton
+          bg="transparent"
+          _hover={{
+            bg: 'grey.600:alpha.20',
+          }}
+          colorScheme="grey"
+          rounded="xl"
+          icon={(
+            <Icon
+              size="xl"
+              as={MaterialIcons}
+              name="close"
+              color="black"
+            />
+          )}
+          onPress={() => router.back()}
+        />
+      </Box>
     </Box>
   )
 }

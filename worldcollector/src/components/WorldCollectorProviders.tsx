84a471b8b3c189dc086eedb5f3c0a09efcce4ea3
David Hérault <dherault@gmail.com>
2023-06-08 22:21:21 +0200

    Remove old files

--- worldcollector/src/components/WorldCollectorProviders.tsx
+++ worldcollector/src/components/WorldCollectorProviders.tsx
@@ -1,20 +1,12 @@
-import { ReactNode, useMemo, useState } from 'react'
-
-import RouteTitlesContext from '~contexts/RouteTitlesContext'
+import { ReactNode } from 'react'
 
 type WorldCollectorProvidersProps = {
   children: ReactNode
 }
 
 function WorldCollectorProviders({ children }: WorldCollectorProvidersProps) {
-  const [routeTitles, setRouteTitles] = useState<Record<string, string>>({})
-  const routeTitlesContextValue = useMemo(() => ({ routeTitles, setRouteTitles }), [routeTitles])
 
-  return (
-    <RouteTitlesContext.Provider value={routeTitlesContextValue}>
-      {children}
-    </RouteTitlesContext.Provider>
-  )
+  return children as JSX.Element
 }
 
 export default WorldCollectorProviders

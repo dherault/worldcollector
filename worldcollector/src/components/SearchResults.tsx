ae5a2bba0f99370b3de4bcbe643d8a67ed41a482
David Hérault <dherault@gmail.com>
2023-06-11 09:29:24 +0200

    Link search with collectibles

--- worldcollector/src/components/SearchResults.tsx
+++ worldcollector/src/components/SearchResults.tsx
@@ -1,25 +1,38 @@
-import { Box, FlatList, Text } from 'native-base'
-import { forwardRef } from 'react'
+import { forwardRef, useCallback } from 'react'
+import { Box, FlatList, Pressable, Text } from 'native-base'
 import { useInfiniteHits } from 'react-instantsearch-hooks'
+import { useRouter } from 'expo-router'
 
 function SearchResults(props, ref) {
   const { hits, isLastPage, showMore } = useInfiniteHits(props)
+  const router = useRouter()
+
+  const handlePress = useCallback(item => {
+    router.push(item.description ? `/-/${item.id}` : '/')
+  }, [router])
 
   return (
     <FlatList
       ref={ref}
       data={hits}
       keyExtractor={item => item.objectID}
-      ItemSeparatorComponent={() => <Box borderBottomColor="grey.500" />}
+      ItemSeparatorComponent={() => (
+        <Box
+          borderBottomColor="grey.100"
+          borderBottomWidth={1}
+        />
+      )}
       onEndReached={() => {
         if (!isLastPage) {
           showMore()
         }
       }}
       renderItem={({ item }) => (
-        <Box p={2}>
-          <Text>{item.name as string}</Text>
-        </Box>
+        <Pressable onPress={() => handlePress(item)}>
+          <Box p={2}>
+            <Text>{item.name as string}</Text>
+          </Box>
+        </Pressable>
       )}
     />
   )

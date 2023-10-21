0969e64189b4173dcb28c4b3e6ae6b5d53cf3258
David Hérault <dherault@gmail.com>
2023-10-21 16:27:56 +0200

    Implement search box and results

--- worldcollector/app/_layout.tsx
+++ worldcollector/app/_layout.tsx
@@ -11,8 +11,8 @@ import TabBar from '~components/TabBar'
 import '~firebase'
 
 const searchClient = algoliasearch(
-  process.env.ALGOLIA_APP_ID,
-  process.env.ALGOLIA_API_KEY
+  process.env.EXPO_PUBLIC_ALGOLIA_APP_ID,
+  process.env.EXPO_PUBLIC_ALGOLIA_API_KEY
 )
 
 function RootLayout() {
---
+++ worldcollector/src/components/SearchBox.tsx
@@ -0,0 +1,54 @@
+import { useCallback, useRef, useState } from 'react'
+import { StyleSheet, TextInput } from 'react-native'
+import { useSearchBox } from 'react-instantsearch-core'
+
+import theme from '~theme'
+
+function SearchBox({ onChange }) {
+  const { query, refine } = useSearchBox()
+  const [inputValue, setInputValue] = useState(query)
+  const inputRef = useRef(null)
+
+  const setQuery = useCallback(newQuery => {
+    setInputValue(newQuery)
+    refine(newQuery)
+    onChange()
+  }, [refine, onChange])
+
+  // Track when the InstantSearch query changes to synchronize it with
+  // the React state.
+  // We bypass the state update if the input is focused to avoid concurrent
+  // updates when typing.
+  if (query !== inputValue && !inputRef.current?.isFocused()) {
+    setInputValue(query)
+  }
+
+  return (
+    <TextInput
+      autoFocus
+      ref={inputRef}
+      value={inputValue}
+      onChangeText={setQuery}
+      clearButtonMode="while-editing"
+      autoCapitalize="none"
+      autoCorrect={false}
+      spellCheck={false}
+      autoComplete="off"
+      placeholder="Start typing to search..."
+      style={styles.root}
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+  root: {
+    paddingHorizontal: 16,
+    paddingVertical: 16,
+    fontSize: 16,
+    borderBottomColor: theme.colors.grey[100],
+    borderBottomWidth: 1,
+    borderBottStyle: 'solid',
+  },
+})
+
+export default SearchBox

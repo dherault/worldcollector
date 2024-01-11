bb4684350b857e9700b7ad10cd493ab7482ebdf4
David Hérault <dherault@gmail.com>
2024-01-11 13:46:46 +0100

    Add rules to authentication

--- worldcollector/app/authentication.tsx
+++ worldcollector/app/authentication.tsx
@@ -9,8 +9,8 @@ import SafeLayout from '~components/SafeLayout'
 function Page() {
   const [step, setStep] = useState(0)
 
-  const handleStartGamePress = useCallback(() => {
-    setStep(1)
+  const handleNext = useCallback(() => {
+    setStep(x => x + 1)
   }, [])
 
   const renderStepZero = useCallback(() => (
@@ -19,43 +19,86 @@ function Page() {
         <View style={styles.grow} />
         <View style={styles.buttonContainer}>
           <FadeInView>
-            <ButtonPrimaryLarge onPress={handleStartGamePress}>
+            <ButtonPrimaryLarge onPress={handleNext}>
               Start game
             </ButtonPrimaryLarge>
           </FadeInView>
         </View>
       </SafeLayout>
     </CustomImageBackground>
-  ), [handleStartGamePress])
+  ), [handleNext])
 
   const renderStepOne = useCallback(() => (
     <CustomImageBackground source={require('../assets/splash-nologo.png')}>
       <SafeLayout>
         <View style={styles.mainContainer}>
-          <Text style={styles.text}>
-            World Collector is a game about collecting elements from the world.
+          <View style={styles.grow} />
+          <Text style={styles.ruleHeader}>
+            Rule #1
           </Text>
-          <Text style={[styles.text, styles.marginTop]}>
-            The rules are simple:
+          <Text style={styles.ruleContent}>
+            You can collect anything except living beings.
           </Text>
-          <Text style={[styles.text, styles.marginTop]}>
-            1 - You can collect something only once. If another player has it you cannot have it.
-          </Text>
-          <Text style={[styles.text, styles.marginTop]}>
-            2 - You cannot collect living beings.
-          </Text>
-          <Text style={[styles.text, styles.marginTop]}>
-            3 - If you spend 30 days without login in your collectible are released.
-          </Text>
+          <View style={styles.grow} />
+          <View style={styles.buttonContainer}>
+            <ButtonPrimaryLarge onPress={handleNext}>
+              Got it!
+            </ButtonPrimaryLarge>
+          </View>
         </View>
       </SafeLayout>
     </CustomImageBackground>
-  ), [])
+  ), [handleNext])
+
+  const renderStepTwo = useCallback(() => (
+    <CustomImageBackground source={require('../assets/splash-nologo.png')}>
+      <SafeLayout>
+        <View style={styles.mainContainer}>
+          <View style={styles.grow} />
+          <Text style={styles.ruleHeader}>
+            Rule #2
+          </Text>
+          <Text style={styles.ruleContent}>
+            After 30 days without login in, your collectibles will be lost.
+          </Text>
+          <View style={styles.grow} />
+          <View style={styles.buttonContainer}>
+            <ButtonPrimaryLarge onPress={handleNext}>
+              Understood!
+            </ButtonPrimaryLarge>
+          </View>
+        </View>
+      </SafeLayout>
+    </CustomImageBackground>
+  ), [handleNext])
+
+  const renderStepThree = useCallback(() => (
+    <CustomImageBackground source={require('../assets/splash-nologo.png')}>
+      <SafeLayout>
+        <View style={styles.mainContainer}>
+          <View style={styles.grow} />
+          <Text style={styles.ruleHeader}>
+            Rule #3
+          </Text>
+          <Text style={styles.ruleContent}>
+            Pressing "like" on a collectible gives you a chance to acquire it if its owner does not log in for 30 days.
+          </Text>
+          <View style={styles.grow} />
+          <View style={styles.buttonContainer}>
+            <ButtonPrimaryLarge onPress={handleNext}>
+              Let's begin!
+            </ButtonPrimaryLarge>
+          </View>
+        </View>
+      </SafeLayout>
+    </CustomImageBackground>
+  ), [handleNext])
 
   switch (step) {
     case 0: return renderStepZero()
-
     case 1: return renderStepOne()
+    case 2: return renderStepTwo()
+    case 3: return renderStepThree()
   }
 }
 
@@ -76,13 +119,17 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
   },
-  text: {
+  ruleHeader: {
     color: 'white',
-    fontSize: 18,
+    fontSize: 42,
+    fontWeight: 'bold',
     textAlign: 'center',
   },
-  marginTop: {
-    marginTop: 16,
+  ruleContent: {
+    marginTop: 32,
+    color: 'white',
+    fontSize: 32,
+    textAlign: 'center',
   },
 })
 
